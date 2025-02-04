65380ec33129bf4cc7c6a68f2a178881196aad4d
321Proteus <bgproteus@gmail.com>
2025-02-04 13:27:48 +0100

    Added variable editor width

--- app/Editor.tsx
+++ app/Editor.tsx
@@ -1,51 +1,45 @@
 import { useEffect, useRef, useState } from "react";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import useEditorContext from "./Context";
 
 function Editor() {
 
-    const editorRef = useRef<HTMLTextAreaElement | null>(null);
     const sliderRef = useRef<HTMLInputElement | null>(null);
 
-    const docWidth = document.documentElement.clientWidth
-
-    const [editorWidth, setEditorWidth] = useState(docWidth * .4)
-    const { text } = useEditorContext();
+    const docWidth = document.documentElement.clientWidth;
+    const [editorWidth, setEditorWidth] = useState(docWidth * .4);
+    const { editor } = useEditorContext();
 
     useEffect(() => {
+        const textarea = editor.current!;
+        const slider = sliderRef.current!;
 
-        const textarea = editorRef.current!
-        const slider = sliderRef.current!
+        // textarea.oninput = () => {
+        //     text.current = textarea.value;
+        // };
 
-        textarea.oninput = () => {
-            text.current = textarea.value;
-        }
         slider.oninput = () => {
-            textarea.style.width = slider.value;
-            console.log(parseInt(slider.value));
-            setEditorWidth(docWidth * parseInt(slider.value) / 100);
-        }
-    })
+            const sliderValue = parseInt(slider.value);
+            textarea.style.width = sliderValue + "px";
+            setEditorWidth(docWidth * sliderValue / 100 * .45);
+        };
+    }, [docWidth, text]);
 
     return (
         <div className={styles.editor}>
-
             <div className={styles.heading}>Edytor</div>
             <input
-                ref={sliderRef}
-                width={editorWidth}
-                type="range"
+                ref={sliderRef} type="range"
+                min="0" max="100"
                 className={styles["width-picker"]}
-            ></input>
-
+            />
             <textarea
                 ref={editorRef}
                 className={styles.textarea}
-            >
-
-            </textarea>
+                style={{ width: editorWidth + "px" }}
+            />
         </div>
-    )
+    );
 }
 
 export default Editor;
